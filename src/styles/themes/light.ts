fe587646683f20cb22b174eb38001d7738022441
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2023-01-11 09:22:45 -0300

    feat: configured styled-components

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,35 @@
+export const light = {
+  colors: {
+    primary: {
+      300: '#F1E9C9',
+      500: '#DBAC2C',
+      700: '#C47F17'
+    },
+    secondary: {
+      300: '#EBE5F9',
+      400: '#d5c2fd',
+      500: '#8047F8',
+      700: '#4B2995'
+    },
+
+    baseTitle: '#272221',
+    baseSubtitle: '#403937',
+    baseText: '#574F4D',
+    baseLabel: '#8D8686',
+
+    baseHover: '#D7D5D5',
+    baseButton: '#E6E5E5',
+    baseinput: '#EDEDED',
+    baseCard: '#F3F2F2',
+
+    background: '#FAFAFA',
+    neutral: '#FFFFFF'
+  },
+
+  fonts: {
+    family: {
+      roboto: "'Roboto', sans-serif",
+      baloo2: "'Baloo 2', cursive"
+    }
+  }
+}
